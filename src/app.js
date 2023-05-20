dec3a45edac4c4b5578dafc1d42bdb363d79bafe
Pasindu Lakshan <dkp9912@gmail.com>
2023-05-20 11:56:25 +0530

    Fixx

--- src/app.js
+++ src/app.js
@@ -71,5 +71,5 @@ global.__basedir = __dirname
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-  console.log(`Server successfully started on port ${port}`)
+  logger.info(`Server successfully started on port ${port}`)
 })
